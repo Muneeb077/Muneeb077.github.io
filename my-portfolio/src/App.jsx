bb9630d30621d83f93d44b7f64f6d188eb0d25c9
Muneeb Ur Rehman <urehman@mun.ca>
2025-09-17 23:29:38 -0230

    Updated the column section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,7 +6,7 @@ import { projects } from './data/projects'
 const App = () => {
   return (
     <MainPageLayout>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
         {projects.map(project => (
           <ProjectCard
             key={project.id}
